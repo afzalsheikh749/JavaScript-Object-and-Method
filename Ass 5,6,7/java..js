cc2851a84bd46fde3e103dc7cd5d0f0f2654e640
Afzal Hussain <mdafzal297922@gmail.com>
2025-10-02 16:34:16 +0500

    This is a Javascript assignment

---
+++ Ass 5,6,7/java..js
@@ -0,0 +1,118 @@
+console.log("welcome to JS");
+
+
+
+// Chapter No # 05
+
+// // Q No 1
+
+// var num1 = 3 ;
+// var num2 = 5 ;
+// var add =  num1 + num2 ;
+
+// document.write(" Sum of " + num1 + " and " + num2 + " is " + add + "<br>");
+
+
+// // Q No 02
+
+ 
+// //    (-)
+//  var num1 = 3;
+//  var num2 = 5;
+
+//  var sub = num1 - num2;
+//  document.write("Subtraction: " + sub + "<br>");
+
+//     // (*)
+//  var num1 = 3;
+//  var num2 = 5;
+
+//  var multiply = num1 * num2;
+// document.write("Multification: " + multiply + "<br>");
+
+//     // (/)
+//  var num1 = 3;
+//  var num2 = 5;
+
+//  var divide = num1 / num2;
+//  document.write("Division: " + divide + "<br>");
+
+//     // modulus (%)
+//  var num1 = 3;
+//  var num2 = 5;
+
+//  var modulus = num1 % num2;
+
+//  document.write("Modulus: " + modulus + "<br><br><br>");
+
+// //  Q No 03
+
+// var declare = 2 ;
+// document.write("Value after varriable declaration is " + declare + "<br>");
+ 
+// document.write("Initial Value : 5 " + "<br>");
+// document.write("Value after increment is : 6 "+ "<br>");  
+ 
+// var value = 5 ;
+// var add   = 6;
+ 
+// var addition = declare + value + add ;
+// console.log(addition);
+// document.write("Value after addition is : " + addition +"<br>");
+
+// var value = 5 ;
+// var add   = 6;
+// var dar  = 1;
+// var Dec = value + add + dar ;
+
+// document.write("Value after decrement is : " + Dec +"<br>");
+
+// var remind = 0.6 /3 ;
+ 
+// console.log(remind);
+// document.write("The Reminder is : 0" + "<br><br>");
+
+// // q No 04
+
+// var ticketPrice = 600 ;
+// var qantity = 5 ;
+
+// var multiply = ticketPrice * qantity ;
+
+// console.log(multiply);
+
+// document.write("Total cost to buy " + qantity + " " + "tickets to a movie is 3000 PKR"+ "<br><br>");
+
+// // Q No # 05
+
+// document.write("Table of 4" + "<br>");
+// document.write("4x1=4" + "<br>" +  "4x2=8" + "<br>" + "4x3=12" + "<br>" + "4x4=16" + "<br>"  + "4x5=20" + "<br>" 
+//  + "4x6=24" + "<br>" + "4x7=28" + "<br>" + "4x8=32" + "<br>" + "4x9=36" + "<br>" + "4x10=40" + "<br>");
+ 
+// //  Q No # 06
+
+//  var calCius = 25 ;
+//  var farhen = (calCius * 9 / 5 )  + 32 ;
+
+//  document.write(calCius + " C " + "  is " + farhen + " F " + " <Br> ")
+
+//  var farhen = 70;
+//  var calCius = (farhen - 32) * 5 / 9 ;
+
+//  document.write(farhen + " F " + "  is " + calCius + " C " + " <Br> ")
+
+// Q No # 07
+
+// var price1 = 650 ;
+// var price2 = 100 ;
+// var quantity1 =  3 ;
+// var quantity2 =  7 ;
+// var charge = 100 ;
+// var total = (price1*quantity1) + (price2*quantity2 ) + charge;
+
+// document.write("Price of item 1 is " + price1 + "<br>"  );
+// document.write("Quantity of item 1 is " + quantity1 + "<br>"  );
+// document.write("Price of item 2 is " + price2 + "<br>"  );
+// document.write("Quantity of item 2 is " + quantity2 +"<br>" + "Shipping charges" +  charge + "<br>" + "<br>");
+// document.write("Total cost of your order is " + total);
+
