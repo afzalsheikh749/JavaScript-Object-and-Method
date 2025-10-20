aeb32e760018443a404aeeb386522995f90d39e0
Afzal Hussain <mdafzal297922@gmail.com>
2025-10-20 14:02:52 +0500

    This is Advance calculator

---
+++ Calculator/java.js
@@ -0,0 +1,54 @@
+var display = document.getElementById("display");
+
+
+console.log(display);
+
+
+function press(val){
+    display.value += val;
+}
+
+function cleardisplay(){
+    display.value = ""
+}
+
+function delChar(){
+    display.value = display.value.slice(0, -1)   
+}
+
+function calculate(){
+    var text = display.value;
+    var opt = "+-*/%."
+
+    if(text === ""){
+        display.value = "";
+        alert("Please type an number and operator")
+        return
+    }else if(opt.includes(text[0]) || opt.includes(text[text.length -1])){
+        display.value = "Error";
+        return       
+    }
+
+    var errorFound = false;
+
+    for(var i= 0; i < text.length -1; i++){
+        if(opt.includes(text[i]) && opt.includes(text[i + 1])){
+            errorFound = true;
+            break;
+        }
+    }
+    
+    if(errorFound){
+        display.value = "Error";
+    }else{
+        display.value = eval(text)
+    }
+
+
+
+}
+
+// var data = "Hello World!";
+
+//  console.log(data.length -1);
+// console.log(data.includes("Hello"));
